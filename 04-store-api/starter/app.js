bf36eefb95a1af37a2865849ed0d2fefea35573a
Wanjie Ryan <104978200+Wanjie-Ryan@users.noreply.github.com>
2023-01-17 23:24:33 +0300

    store-api

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,58 @@
-console.log('04 Store API')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const notfound = require('./middleware/not-found')
+// const errorhandler = require('./middleware/errorhandler')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+
+
+
+// middlewares
+
+app.use(express.json())
+
+//routes
+
+app.get('/', (req, res)=>{
+    res.send('<h1> Store API </h1><a href ="/api/v1/products">Products</a')
+})
+
+
+// products route
+
+app.use(notfound)
+
+
+const DB = async()=>{
+
+    try{
+
+        // connecting to the DB
+        await connectDB(process.env.mongoDB);
+        app.listen({port}, ()=>{
+            console.log(`server is listneing on port,${port}`)
+        })
+
+    }
+
+    catch(error){
+
+        console.log(error)
+    }
+
+
+
+}
+
+DB()
+
+
+
+
+
+
+
+
+
