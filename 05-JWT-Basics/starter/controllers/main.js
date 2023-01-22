bdee938869c613625095962e259c2698737f3da0
Wanjie Ryan <104978200+Wanjie-Ryan@users.noreply.github.com>
2023-01-22 17:06:42 +0300

    creating a token

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,7 +1,55 @@
+// first access the username and password using the req.body
+// if both of the above are available, then create a new JWT
+
+//set up an authentication whereby only the request with JWT can access the dashboard
+
+
+// first import the jwt package
+const jwt = require('jsonwebtoken')
+
 const login  = async(req, res) =>{
 
-    res.send('Sample login/Register')
+    try{
+
+        const {username, password} = req.body
+        console.log(username, password)
+
+        if(!username || !password){
+
+            res.status(400).json({msg: 'Please provide the full details'})
+        }
+
+        // temporary id, as theere is no DB at the moment
+
+        const id = new Date().getDate()
+
+        //sign method takes on 3 values: 1. payload, 2. jwt_secret and 3. options
+
+        // 1. PAYLOAD
+        // payload is an object and you can pass in anything you want but not a password that you usually use or confidential information 
+        // payload should be small
+
+        // 2.JWT_SECRET
+        // JWT SECRET is set up in the .env and should a long string that no one should be able to guess
+
+        const token = jwt.sign({id, username }, process.env.jwt_secret,  {expiresIn: '30d'}  )
+
+
+
+
+        res.status(200).json({msg: 'created user', token})
+
+    }
+
+    catch(error){
+
+        res.status(500).json({msg: error})
+    }
+
+
 }
+
+
 
 
 
