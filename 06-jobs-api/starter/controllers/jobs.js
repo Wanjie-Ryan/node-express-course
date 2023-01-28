f8dc82dc7769136e383ff247480ccb7915b84950
Wanjie Ryan <104978200+Wanjie-Ryan@users.noreply.github.com>
2023-01-28 08:50:43 +0300

    finished up on the controllers

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -58,7 +58,12 @@ const createjob = async(req, res)=>{
 
     try{
 
+       // params is provided by express.
+       //jobId is just an alias that is passed as a parameter 
+       //we can access userId because we have access to the token.
       const {user:{userId}, params:{id:jobId}} = req
+
+      //finding the job whose jobid matches it and for specifically that one user who created the job.
 
       const personjobs = await jobs.findOne({_id:jobId, createdBy:userId})
 
@@ -68,7 +73,6 @@ const createjob = async(req, res)=>{
       }
 
       res.status(StatusCodes.OK).json({personjobs})
-
 
 
     }
@@ -83,8 +87,35 @@ const createjob = async(req, res)=>{
   };
 
   const updatejob = async(req, res)=>{
+      
+    try{
 
-    res.send('update job')
+      // destructuring the req functionality into req.user, req.params and req.body
+
+      const {user:{userId}, params:{id:jobId}, body:{company, position}} = req
+
+      if(company == '' || position == ''){
+        res.status(StatusCodes.BAD_REQUEST).json({msg:'Provide the company and position fields!'})
+      }
+
+      const personsjobs = await jobs.findOneAndUpdate({_id:jobId, createdBy:userId}, req.body, {new:true, runValidators:true})
+
+      if(!personsjobs){
+
+        res.status(StatusCodes.NOT_FOUND).json({msg:`person with jobId ${jobId} cannot be found`})
+      }
+
+      res.status(StatusCodes.OK).json({personsjobs})
+
+
+
+    }
+
+    catch(error){
+
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+    }
+
 
 
 
@@ -94,7 +125,33 @@ const createjob = async(req, res)=>{
 
   const deletejob = async(req, res)=>{
 
-    res.send('delete job')
+    try{
+
+      const{user:{userId}, params:{id:jobId}} = req
+
+      const personsjobs = await jobs.findOneAndRemove({_id:jobId, createdBy:userId})
+
+      if(!personsjobs){
+
+        res.status(StatusCodes.BAD_REQUEST).json({msg:`person with the job id${jobId} was not found`})
+
+
+      }
+
+      res.status(StatusCodes.OK).json({personsjobs})
+
+
+
+    }
+
+    catch(error){
+
+
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+
+
+
+    }
 
 
 
